dc80ef9c0eef49517f078e227df780ffefb7b51a
Dee J. Mann <dee.j.mann@hotmail.com>
2025-10-10 14:40:17 -0400

    Changed Android icon to focuspilot_tmp.png Changes to be committed: 	modified:   FocusPilot.UI/FocusPilot.UI.Android/MainActivity.cs

--- FocusPilot.UI/FocusPilot.UI.Android/MainActivity.cs
+++ FocusPilot.UI/FocusPilot.UI.Android/MainActivity.cs
@@ -8,7 +8,7 @@ namespace FocusPilot.UI.Android;
 [Activity(
     Label = "FocusPilot.UI.Android",
     Theme = "@style/MyTheme.NoActionBar",
-    Icon = "@drawable/icon",
+    Icon = "@drawable/focuspilot_tmp",
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 public class MainActivity : AvaloniaMainActivity<App>
